fd1dbff37fb21e2a09de688287aec04c157716d0
MarcosMiranda00 <mm0014032019@unab.edu.sv>
2020-04-11 16:50:49 -0600

    Continuacion 3

--- MarcosMirandaTarea/MarcosMirandaTarea/Vista/frmMantenimientoMaterias.Designer.cs
+++ MarcosMirandaTarea/MarcosMirandaTarea/Vista/frmMantenimientoMaterias.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.btnActualizar = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtvMateria = new System.Windows.Forms.DataGridView();
             this.txtNombreMateria = new System.Windows.Forms.TextBox();
             this.txtId = new System.Windows.Forms.TextBox();
             this.lblNombreMateria = new System.Windows.Forms.Label();
             this.lblId = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Materia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvMateria)).BeginInit();
             this.SuspendLayout();
             // 
             // btnActualizar
@@ -48,6 +50,7 @@
             this.btnActualizar.TabIndex = 13;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnGuardar
             // 
@@ -59,17 +62,26 @@
             this.btnGuardar.TabIndex = 12;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // dataGridView1
+            // dtvMateria
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 198);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(447, 180);
-            this.dataGridView1.TabIndex = 11;
+            this.dtvMateria.AllowUserToAddRows = false;
+            this.dtvMateria.AllowUserToDeleteRows = false;
+            this.dtvMateria.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtvMateria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvMateria.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Materia});
+            this.dtvMateria.Location = new System.Drawing.Point(23, 198);
+            this.dtvMateria.Margin = new System.Windows.Forms.Padding(2);
+            this.dtvMateria.Name = "dtvMateria";
+            this.dtvMateria.ReadOnly = true;
+            this.dtvMateria.RowHeadersWidth = 51;
+            this.dtvMateria.RowTemplate.Height = 24;
+            this.dtvMateria.Size = new System.Drawing.Size(505, 180);
+            this.dtvMateria.TabIndex = 11;
+            this.dtvMateria.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvMateria_CellClick);
             // 
             // txtNombreMateria
             // 
@@ -81,6 +93,7 @@
             // 
             // txtId
             // 
+            this.txtId.Enabled = false;
             this.txtId.Location = new System.Drawing.Point(23, 42);
             this.txtId.Margin = new System.Windows.Forms.Padding(2);
             this.txtId.Name = "txtId";
@@ -109,21 +122,34 @@
             this.lblId.TabIndex = 7;
             this.lblId.Text = "Id";
             // 
-            // frmMantenimientoNotas
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // Materia
+            // 
+            this.Materia.HeaderText = "Materia";
+            this.Materia.Name = "Materia";
+            this.Materia.ReadOnly = true;
+            // 
+            // frmMantenimientoMaterias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 450);
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtvMateria);
             this.Controls.Add(this.txtNombreMateria);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.lblNombreMateria);
             this.Controls.Add(this.lblId);
-            this.Name = "frmMantenimientoNotas";
+            this.Name = "frmMantenimientoMaterias";
             this.Text = "frmMantenimientoNotas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmMantenimientoMaterias_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvMateria)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,10 +159,12 @@
 
         private System.Windows.Forms.Button btnActualizar;
         private System.Windows.Forms.Button btnGuardar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtvMateria;
         private System.Windows.Forms.TextBox txtNombreMateria;
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Label lblNombreMateria;
         private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Materia;
     }
 }
--- MarcosMirandaTarea/MarcosMirandaTarea/Vista/frmMantenimientoMaterias.cs
+++ MarcosMirandaTarea/MarcosMirandaTarea/Vista/frmMantenimientoMaterias.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MarcosMirandaTarea.Model;
+using MarcosMirandaTarea.Vista;
 
 namespace MarcosMirandaTarea.Vista
 {
@@ -15,6 +17,72 @@ namespace MarcosMirandaTarea.Vista
         public frmMantenimientoMaterias()
         {
             InitializeComponent();
+        }
+        public void limpiar()
+        {
+            txtId.Text = "";
+            txtNombreMateria.Text = "";
+            
+        }
+        public void CargarDatos()
+        {
+            using (notasEstudiantesEntities db = new notasEstudiantesEntities())
+            {
+                var mat1 = db.materia;
+                foreach (var iterar in mat1)
+                {
+                    dtvMateria.Rows.Add(iterar.id_materia, iterar.nombre_materia);
+                }
+            }
+        }
+
+        materia mat = new materia();
+        
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (notasEstudiantesEntities db = new notasEstudiantesEntities())
+            {
+                mat.nombre_materia = txtNombreMateria.Text;
+                              
+                db.materia.Add(mat);
+                db.SaveChanges();
+            }
+            dtvMateria.Rows.Clear();
+            limpiar();
+            CargarDatos();
+        }
+
+        private void dtvMateria_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string Id = dtvMateria.CurrentRow.Cells[0].Value.ToString();
+            string nombreMateria = dtvMateria.CurrentRow.Cells[1].Value.ToString();
+
+            txtId.Text = Id;
+            txtNombreMateria.Text = nombreMateria;
+        }
+
+        private void frmMantenimientoMaterias_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            using (notasEstudiantesEntities db = new notasEstudiantesEntities())
+            {
+                string Id = dtvMateria.CurrentRow.Cells[0].Value.ToString();
+                int Idc = int.Parse(Id);
+                mat = db.materia.Where(verificarId => verificarId.id_materia == Idc).First();
+                mat.nombre_materia = txtNombreMateria.Text;
+                
+
+                db.Entry(mat).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            dtvMateria.Rows.Clear();
+            limpiar();
+            CargarDatos();
         }
     }
 }
